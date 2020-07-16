7283f2a6f7d792565ad31eff4bba6bec08141669
ohall <ohall@grassroots-tech.com>
2020-07-16 11:06:52 -0400

    cleanup

--- sprinkler.js
+++ sprinkler.js
@@ -1,14 +1,38 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const git = require('nodegit');
+const {Clone, Reference, Signature, Remote, Cred} = require('nodegit');
 const ships = require('culture-ships');
+const rimraf = require('rimraf');
+const fileName = 'ships.txt';
+const author = Signature.now('Beep Boop', 'beepn@boop.com');
+const committer = Signature.now('Beep Boop', 'beepn@boop.com');
 
+const repoURL = process.argv[2];
+const repoUsername = process.argv[3];
+const repoPassword = process.argv[4];
 
 const go = async function() {
-  const repo = await git.Clone("https://github.com/ohall/sprinkler", "./tmp");
-  fs.writeFileSync('tmp/sprinkler/ships.txt', ships.random(), { flag: 'wx' });
+  const msg = ships.random();
+  try {
+     rimraf.sync('./tmp');
+    const repo = await Clone(repoURL, './tmp');
+    fs.appendFileSync(`./tmp/${fileName}`, `${msg}\n` );
+    const index = await repo.refreshIndex();
+    await index.addByPath(fileName);
+    await index.write();
+    const oid = await index.writeTree();
+    const head = await Reference.nameToId(repo, 'HEAD');
+    const parent = await repo.getCommit(head);
+    await repo.createCommit('HEAD', author, committer, msg, oid, [parent]);
+    const remote = await Remote.lookup(repo, 'origin');
+    const credentials = () => Cred.userpassPlaintextNew(repoUsername, repoPassword);
+    await remote.push(['refs/heads/master:refs/heads/master'], { callbacks: { credentials } });
+  } catch (e) {
+    console.error( 'Commit and push operation failed' );
+    console.error( e );
+    return 1;
+  }
+  console.log( `Commit ${msg} pushed` );
 };
 
-
 go();
-
